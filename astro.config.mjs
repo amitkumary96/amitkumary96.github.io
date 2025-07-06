0c364196f9e867d99dd4e05734707c1842d91379
amitkumary96 <43061461+amitkumary96@users.noreply.github.com>
2025-07-06 17:28:30 +0530

    Update astro.config.mjs
    Remove Import and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { astroContent } from 'astro:content';
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'dracula'
